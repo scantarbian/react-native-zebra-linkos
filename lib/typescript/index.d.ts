ba4775f1b2aead86bb946b65a3c1bb4cd656356c
scantarbian <davidpamereanb@gmail.com>
2023-04-20 22:13:24 +0700

    fix: show generated libs

---
+++ lib/typescript/index.d.ts
@@ -0,0 +1,3 @@
+export declare function multiply(a: number, b: number): Promise<number>;
+export declare function writeTCP(ipAddress: string, zpl: string): Promise<boolean>;
+//# sourceMappingURL=index.d.ts.map
