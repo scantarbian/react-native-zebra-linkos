5f79abe7231b0fc0348b0cefa1f61a96e13d5215
scantarbian <davidpamereanb@gmail.com>
2023-04-24 14:57:19 +0700

    fix: update prepack module

--- lib/typescript/index.d.ts
+++ lib/typescript/index.d.ts
@@ -1,3 +1,4 @@
 export declare function multiply(a: number, b: number): Promise<number>;
 export declare function writeTCP(ipAddress: string, zpl: string): Promise<boolean>;
+export declare function scanNetwork(): Promise<string[]>;
 //# sourceMappingURL=index.d.ts.map
