3dcc7be79d789a1f85482c369f78b23f81bb1fc4
scantarbian <davidpamereanb@gmail.com>
2024-07-29 15:11:08 +0700

    feat: scaffold functions

--- src/NativeZebraLinkos.ts
+++ src/NativeZebraLinkos.ts
@@ -3,6 +3,9 @@ import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
   multiply(a: number, b: number): number;
+  writeTCP(ipAddress: string, zpl: string): Promise<boolean>;
+  scanNetwork(): Promise<string[]>;
+  scanBluetooth(): Promise<string[]>;
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>('ZebraLinkos');
--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,18 @@ const ZebraLinkos = require('./NativeZebraLinkos').default;
 export function multiply(a: number, b: number): number {
   return ZebraLinkos.multiply(a, b);
 }
+
+export async function writeTCP(
+  ipAddress: string,
+  zpl: string
+): Promise<boolean> {
+  return await ZebraLinkos.writeTCP(ipAddress, zpl);
+}
+
+export async function scanNetwork(): Promise<string[]> {
+  return await ZebraLinkos.scanNetwork();
+}
+
+export async function scanBluetooth(): Promise<string[]> {
+  return await ZebraLinkos.scanBluetooth();
+}
