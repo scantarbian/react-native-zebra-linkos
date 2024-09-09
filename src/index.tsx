07e36a02aebf140380af63936072dde5122589ac
scantarbian <davidpamereanb@gmail.com>
2024-09-09 18:42:54 +0700

    fix: add linking error screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,34 @@
-const ZebraLinkos = require('./NativeZebraLinkos').default;
 import type {
   DiscoveredPrinter,
   DiscoveredPrinterBluetooth,
   DiscoveredPrinterBluetoothLe,
   DiscoveredPrinterNetwork,
 } from './@types/index';
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'react-native-zebra-linkos' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+
+// @ts-expect-error
+const isTurboModuleEnabled = global.__turboModuleProxy != null;
+
+const ZebraLinkosModule = isTurboModuleEnabled
+  ? require('./NativeZebraLinkos').default
+  : NativeModules.ZebraLinkos;
+
+const ZebraLinkos = ZebraLinkosModule
+  ? ZebraLinkosModule
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
 
 export async function writeTCP(
   ipAddress: string,
